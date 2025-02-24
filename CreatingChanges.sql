020108e74b8e9a22f237290d57dd116f4703c3e3
Levid Lima <120687641+DEVLevid@users.noreply.github.com>
2025-02-24 17:09:20 -0300

    Update CreatingChanges.sql

--- CreatingChanges.sql
+++ CreatingChanges.sql
@@ -3,7 +3,6 @@ CREATE INDEX idx_order_date ON `order`(order_purchase_timestamp);
 CREATE INDEX idx_order_product_date ON order_item(order_id, product_id);
 CREATE INDEX idx_order_delivery_date ON `order`(order_delivered_customer_date);
 
--- 4.1
 EXPLAIN ANALYZE
 SELECT s.seller_id, SUM(op.payment_value) AS total_vendas
 FROM seller s
@@ -12,8 +11,6 @@ JOIN order_payment op ON oi.order_id = op.order_id
 GROUP BY s.seller_id
 ORDER BY total_vendas DESC;
 
-
--- 4.2
 EXPLAIN ANALYZE
 SELECT c.customer_id, SUM(op.payment_value) AS total_gasto
 FROM customer c
@@ -25,7 +22,6 @@ GROUP BY c.customer_id
 ORDER BY total_gasto DESC
 LIMIT 10;
 
--- 4.3 
 EXPLAIN ANALYZE
 SELECT oi.seller_id, AVG(orw.review_score) AS media_avaliacao
 FROM order_review orw
@@ -34,8 +30,6 @@ JOIN order_item oi ON o.order_id = oi.order_id
 GROUP BY oi.seller_id
 ORDER BY media_avaliacao DESC;
 
-
--- 4.4
 EXPLAIN ANALYZE
 SELECT o.order_id, c.customer_id, o.order_status, op.payment_value AS total_pago
 FROM `order` o
@@ -43,8 +37,7 @@ JOIN customer c ON o.customer_id = c.customer_id
 JOIN order_payment op ON o.order_id = op.order_id
 WHERE o.order_purchase_timestamp >= '2017-01-01' 
       AND o.order_purchase_timestamp < '2018-01-01'; 
-      
--- 4.5
+
 EXPLAIN ANALYZE
 SELECT oi.product_id, COUNT(*) AS total_vendas
 FROM order_item oi
@@ -55,8 +48,6 @@ GROUP BY oi.product_id
 ORDER BY total_vendas DESC
 LIMIT 5;
 
-
--- 4.6
 EXPLAIN ANALYZE
 SELECT o.order_id, 
        TIMESTAMPDIFF(DAY, o.order_estimated_delivery_date, o.order_delivered_customer_date) AS atraso_dias
@@ -65,7 +56,6 @@ WHERE o.order_delivered_customer_date > o.order_estimated_delivery_date
 ORDER BY atraso_dias DESC
 LIMIT 10;
 
--- 4.7
 EXPLAIN ANALYZE
 SELECT o.customer_id, SUM(op.payment_value) AS total_gasto
 FROM `order` o
@@ -74,7 +64,6 @@ GROUP BY o.customer_id
 ORDER BY total_gasto DESC
 LIMIT 10;
 
--- 4.8
 EXPLAIN ANALYZE
 SELECT c.customer_state, 
        AVG(TIMESTAMPDIFF(DAY, o.order_purchase_timestamp, o.order_delivered_customer_date)) AS media_tempo_entrega
